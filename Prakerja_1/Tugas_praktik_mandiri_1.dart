c20a532500c35024694428d35a6b19f9111f7c54
ngadimin51 <poconggoyangkayang@gmail.com>
2023-07-21 17:32:41 +0700

    Simpan tugas praktik mandiri

---
+++ Prakerja_1/Tugas_praktik_mandiri_1.dart
@@ -0,0 +1,61 @@
+
+void main() {
+  // rumus keliling persegi
+  double sisi = 4.0;
+  double kelilingPersegi = 4 * sisi;
+    print("Soal 1\nSisi: $sisi\nKeliling: $kelilingPersegi\n");
+  
+  // rumus keliling persegi panjang
+  double panjang = 8.0;
+  double lebar = 3.0;
+  double kelilingPersegiPanjang = 2 * (panjang + lebar);
+  print("Soal 2\nPanjang: $panjang\nLebar: $lebar\nKeliling: $kelilingPersegiPanjang\n");
+  
+  // keliling lingkaran
+  double jariJari = 7.0;
+  double phi = 3.1;
+  double kelilingLingkaran = 2 * jariJari * phi;
+  print("Soal 3\nJari²: $jariJari\nKeliling: $kelilingLingkaran");
+  
+  print("Soal 4: Check comment pada code");
+  // tuliskan command² yang ada pada flutter CLI
+  // dan berikan fungsi command tersebut
+  
+  // Flutter doctor
+  //    ● Perintah untuk menampilkan informasi software yang dibutuhkan flutter
+  
+  // Flutter Create
+  //    ● Perintah untuk membuat project aplikasi flutter baru
+  //       di directory tertentu
+  
+  // Flutter Run
+  //    ● Perintah untuk menjalankan project aplikasi di device
+  //       yang tersedia
+  
+  // Flutter Emulator
+  //    ● Perintah untuk menampilkan daftar emulator yang terinstall dan
+  //       menampilkan option untuk membuka emulator atau membuat
+  //       emulator baru
+  
+  // Flutter Channel
+  //    ● Perintah untuk menampilkan daftar flutter channel yang tersedia
+  //       dan menunjukan channel yang digunakan saat ini.
+  
+  // Flutter Pub
+  // Ada dua syntax yang bisa kita gunakan, yaitu:
+  //    ● Flutter pub add, untuk menambahkan packages ke
+  //      dependencies yang ada di pubspec.yaml
+  //      flutter pub add <package_name>
+  //    ● Flutter pub get, untuk mendownload semua packages atau
+  //      dependencies yang ada di pubspec.yaml
+  //      flutter pub get
+  
+  // Flutter Build
+  //    ● Perintah untuk memproduksi sebuah file aplikasi untuk keperluan
+  //      deploy atau publish ke AppStore, PlayStore, dll.
+  
+  // Flutter Clean
+  //   ● Perintah untuk menghapus folder build serta file lainnya yang
+  //     dihasilkan saat kita menjalankan aplikasi di emulator
+  //   ● Perintah ini akan memperkecil ukuran project tersebut.
+}
